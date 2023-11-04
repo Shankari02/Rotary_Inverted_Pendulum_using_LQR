124fab55e9844e79802447b3f8131e1d5d6849ba
Shankari02 <129577901+Shankari02@users.noreply.github.com>
2023-11-05 01:47:05 +0530

    Updated code

--- Hardware/main/main.c
+++ Hardware/main/main.c
@@ -16,21 +16,23 @@
 #define LEDC_MODE LEDC_HIGH_SPEED_MODE
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_10_BIT
+#define update_period 20
 
 // STEP and DIRECTION output pins for stepper motor driver.
 static const gpio_num_t step_pin = GPIO_NUM_14;
 static const gpio_num_t direction_pin = GPIO_NUM_27;
+static const gpio_num_t microstep_pin = GPIO_NUM_10;
 
-//TAG for ESP_LOGI function
+// TAG for ESP_LOGI function
 static const char *TAG = "JMD";
 
-//K is a constant for lqr controller calculated from MATLAB
-float k[4] = {-0.0003, -0.1798, -6.2407, -8.4460};
+// K is a constant for lqr controller calculated from MATLAB
+float k[4] = {-1.0000, -1.4411, -16.4978, -3.4119};
 
-//Setting the stable position
-float state_setpoint[4] = {PI, 0, 0, 0};
+// Setting the stable position
+float state_setpoint[4] = {0, 0, 0, 0};
 
-//defining the variables required
+// defining the variables required
 float u = 0; // the input required to be provided (u = -kx)
 
 // state variables [theta_s  theta_s_dot  theta_e  theta_e_dot]
@@ -41,16 +43,16 @@ float theta_s_dot = 0;
 float theta_e_curr = 0;
 float theta_e_prev = 0;
 float theta_e_dot = 0;
-float frequency = 2;
+float frequency = 5;
 float frequency_change = 0;
 
-//variable for encoder_readings
+// variable for encoder_readings
 int encoder_value;
 
-//error definitions for PID control
+// error definitions for PID control
 float prev_error = 0;
 float difference_error = 0, cumulative_error = 0;
-float kp = 0.5, kd = 0, ki = 0;
+float kp = 0.05, kd = 0, ki = 0;
 
 void set_ledc()
 {
@@ -59,7 +61,7 @@ void set_ledc()
         .speed_mode = LEDC_MODE,
         .timer_num = LEDC_TIMER,
         .duty_resolution = LEDC_DUTY_RES,
-        .freq_hz = frequency,
+        .freq_hz = abs(frequency),
         .clk_cfg = LEDC_AUTO_CLK};
     ledc_timer_config(&ledc_timer);
 
@@ -90,7 +92,7 @@ void set_ledc()
 float calc_error()
 {
     // error = u
-    difference_error = (u - prev_error) * 1000 / (20); // Calculating difference error
+    difference_error = (u - prev_error) * 1000 / (20);         // Calculating difference error
     cumulative_error = cumulative_error + ((u * (20)) / 1000); // Calculatong cumulative error
 
     frequency_change = kp * (u) + ki * (cumulative_error) + kd * (difference_error); // Frequency change
@@ -100,31 +102,32 @@ float calc_error()
 
 float set_freq()
 {
+    gpio_set_level(microstep_pin, 1);
     frequency = frequency + ((frequency_change * (20)) / (1000));
-    ledc_set_freq(LEDC_MODE, LEDC_TIMER, abs(frequency));
+    ledc_set_freq(LEDC_MODE, LEDC_TIMER, 4*abs(frequency));
     return frequency;
 }
 
 void direction()
-{   
+{
     // anticlockwise means -ve freq
     // clockwise means +ve freq
-    if (frequency < 0)
+   
+    if (theta_e_curr > 0)
     {
-        gpio_set_level(direction_pin, 1); // Setting the direction pin high
+        gpio_set_level(direction_pin, 0); // Setting the direction pin high
     }
     else
     {
-        gpio_set_level(direction_pin, 0); // Setting the direction pin low
+        gpio_set_level(direction_pin, 1); // Setting the direction pin low
     }
 }
 
-
-//functions to calculate variables
+// functions to calculate variables
 
 float calculate_theta_s()
 {
-    theta_s_curr = theta_s_curr + (0.036) * (frequency);
+    theta_s_curr = theta_s_curr + ((((0.036) * (frequency)) * PI) / 180);
     return theta_s_curr;
 }
 
@@ -137,26 +140,34 @@ float calculate_theta_s_dot()
 
 float calculate_theta_e()
 {
-    theta_e_curr = theta_e_curr + ((float)encoder_value);
-    return theta_e_curr;
+    if (encoder_value > 0)
+    {
+        theta_e_curr = theta_e_curr + (((float)encoder_value - 180) * PI / 180);
+        return theta_e_curr;
+    }
+    else
+    {
+        theta_e_curr = theta_e_curr + (((float)encoder_value + 180) * PI / 180);
+        return theta_e_curr;
+    }
 }
 
 float calculate_theta_e_dot()
 {
-    theta_e_dot = ((theta_e_curr - theta_e_prev) * (1000))/ (20);
+    theta_e_dot = ((theta_e_curr - theta_e_prev) * (1000)) / (20);
     theta_e_prev = theta_e_curr;
     return theta_e_dot;
 }
 
 float calculate_u()
 {
-    u = -(k[0] * (theta_s_curr - state_setpoint[0]) + k[1] * (theta_s_dot - state_setpoint[1]) + k[2] * (theta_e_curr - state_setpoint[2]) + k[3] * (theta_e_dot - state_setpoint[3]));
+    u = (k[0] * (theta_s_curr - state_setpoint[0]) + k[1] * (theta_s_dot - state_setpoint[1]) + k[2] * (theta_e_curr - state_setpoint[2]) + k[3] * (theta_e_dot - state_setpoint[3]));
     return u;
 }
 
-
-void run_motor()
+void find_angle()
 {
+
     // Rotary encoder underlying device is represented by a PCNT unit in this example
     uint32_t pcnt_unit = 0;
 
@@ -173,12 +184,19 @@ void run_motor()
     // Report counter value
 
     while (1)
-    {   
-        //Printing the Encoder values
-        encoder_value = encoder->get_counter_value(encoder);
-        ESP_LOGI(TAG, "Encoder value: %d", encoder->get_counter_value(encoder));
+    {
+        // Printing the Encoder values
+        encoder_value = (encoder->get_counter_value(encoder)) / 4;
+        ESP_LOGI(TAG, "Encoder value: %d", encoder_value);
+        vTaskDelay(update_period / portTICK_PERIOD_MS);
+    }
+}
+void run_motor()
+{
 
-        // Calling the respective functions to calculate the variables
+    // Calling the respective functions to calculate the variables
+    while (1)
+    {
 
         theta_e_curr = calculate_theta_e();
         theta_e_dot = calculate_theta_e_dot();
@@ -194,9 +212,9 @@ void run_motor()
 
         ESP_LOGI(TAG, "[Theta_s: %f, Theta_s_dot: %f, Theta_e: %f, Theta_e_dot: %f]", theta_s_curr, theta_s_dot, theta_e_curr, theta_e_dot);
         ESP_LOGI(TAG, "U = %f", u);
-    }
 
-    vTaskDelay(pdMS_TO_TICKS(0.2)); //Delay
+        vTaskDelay(update_period / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main()
@@ -204,6 +222,8 @@ void app_main()
 
     set_ledc(); // Setting the ledc configuration
 
-    // Creating task
-    xTaskCreatePinnedToCore(&run_motor, "MOTOR RUNNING", 2048, NULL, 1, NULL, 1);
+    // Creating two tasks : one for finding the angke from the encoder and one for running the motor
+    xTaskCreate(&find_angle, "ENCODER RUNNING", 2048, NULL, 5, NULL);
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    xTaskCreate(&run_motor, "MOTOR RUNNING", 4096, NULL, 1, NULL);
 }
